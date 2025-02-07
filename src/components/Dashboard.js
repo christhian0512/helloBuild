a397fe7f1f73973d7cc4aa7904d2d74b902a9d97
Christhian David Santander Figueroa <christhian0512@gmail.com>
2025-02-07 15:53:15 -0500

    adjust style

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { FaDiscord } from "react-icons/fa";
+import { FaGithub, FaFolder , FaRegFolder  } from "react-icons/fa";
+import { MdFavoriteBorder, MdFavorite } from "react-icons/md";
 import Button from '@mui/material/Button';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import TextField from '@mui/material/TextField';
+import Checkbox from '@mui/material/Checkbox';
+import Divider from '@mui/material/Divider';
+import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+
 
 const Dashboard = () => {
   const [repos, setRepos] = useState([]);
@@ -71,40 +78,72 @@ const Dashboard = () => {
       favorites.includes(repo.id)
     ): [];
 
-    console.log(favoriteRepos);
-
   return (
-    <div>
+    <Box>
       <h2>Dashboard</h2>
-      {!token && (
-      <Button  variant="contained" onClick={handleGitHubLogin}>
-        <FaDiscord size={30} /> Log into GitHub
-      </Button> 
+          {!token && (
+          <Button  variant="contained" onClick={handleGitHubLogin} startIcon={<FaGithub size={20}/>}>
+            Connect Github
+          </Button> 
+          )}
+       {token && (    
+        <Stack
+          spacing={2}
+          direction="row"
+          sx={{
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+          }}
+        >
+          <Box>
+          
+
+              <Box sx={{ p: 2, border: 1, boxShadow: 6, borderColor: 'divider', borderRadius: 1, backgroundColor: '#b6d9f0' }}>
+              <h3>Repositories</h3>
+                <TextField
+                  variant="outlined"
+                  size="small"
+                  type="text"
+                  placeholder="search"
+                  value={searchTerm}
+                  onChange={handleSearch}
+                  sx={{ padding: 1 }}
+                />
+                <Divider />
+                <List>
+                  {results.map((repo) => (
+                    <ListItem key={repo.id} sx={{
+                      justifyContent: "space-between",
+                      alignContent: 'flex-start'
+                    }}>
+                      <ListItemIcon>
+                        <FaRegFolder  />
+                      </ListItemIcon>
+                      <ListItemText>
+                        {repo.name}
+                      </ListItemText>
+                      
+                      <Checkbox icon={<MdFavoriteBorder />} checkedIcon={<MdFavorite />} onClick={() => toggleFavorite(repo)} checked={favorites.includes(repo.id)} />
+                    </ListItem>
+                  ))}
+                </List>
+              </Box>
+            
+          </Box>
+          <Box sx={{ p: 2, border: 1, boxShadow: 6 , borderColor: 'divider', borderRadius: 1, backgroundColor: '#b6d9f0' }}>
+              <h3>Favorites</h3>
+                <Divider />
+                <List>
+                  {favoriteRepos.map((repo) => (
+                    <ListItem key={repo.id}>
+                      {repo.name}
+                    </ListItem>
+                  ))}
+                </List>
+              </Box>
+        </Stack>
       )}
-      {token && (
-        <>
-          <h3>Repositories</h3>
-          <TextField
-            variant="outlined"
-            size="small"
-            type="text"
-            placeholder="search"
-            value={searchTerm}
-            onChange={handleSearch}
-          />
-          <List>
-            {results.map((repo) => (
-              <ListItem key={repo.id}>
-                {repo.name}
-                <Button variant="outlined" onClick={() => toggleFavorite(repo)}>
-                  {favorites.includes(repo.id) ? "Unfavorite" : "Favorite"}
-                </Button>
-              </ListItem>
-            ))}
-          </List>
-        </>
-      )}
-    </div>
+    </Box>
   );
 };
 
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
 
 
 const SignUp = () => {
@@ -16,14 +17,27 @@ const SignUp = () => {
   };
 
   return (
-    <Box>
-      <h2>Sign Up</h2>
-
-        <TextField variant="outlined" size="small" label="Username" type="text" placeholder="Username" value={username} onChange={(event) => setUsername(event.target.value)} />
-        <TextField variant="outlined" size="small" label="Password" type="password" placeholder="Password" value={password} onChange={(event) => setPassword(event.target.value)} />
-        <Button variant="contained" onClick={handleSignUp}>Sign Up</Button>
-
-    </Box>
+    <Stack
+          spacing={1}
+          sx={{
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+      <Box sx={{ p: 2, border: 1, boxShadow: 6, borderColor: 'divider', borderRadius: 1, backgroundColor: '#b6d9f0' }}>
+        <h2>Sign Up</h2>
+        <Stack spacing={2}
+          direction="row"
+          sx={{
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+          }}>
+        <TextField variant="outlined" size="small" label="Username" type="text" value={username} onChange={(event) => setUsername(event.target.value)} />
+          <TextField variant="outlined" size="small" label="Password" type="password"  value={password} onChange={(event) => setPassword(event.target.value)} />
+          <Button variant="contained" onClick={handleSignUp}>Sign Up</Button>
+        </Stack>
+      </Box>
+    </Stack>
   );
 };
 
